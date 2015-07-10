8149aff0a828d8499144b890f32b4c76f3215716
Sarah Paetsch <spaetsch@gmail.com>
2015-07-09 18:33:37 -0700

    Trying to get modules to wrk

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var morgan = require('morgan');             // log requests to the console (expr
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
+
+var routes = require('/routes/index');
 // configuration =================
 
 mongoose.connect('mongodb://localhost/todo-tutorial/data');
@@ -22,62 +24,6 @@ app.use(methodOverride());
   var Todo = mongoose.model('Todo', {
       text : String
   });
-
-
-// routes ======================================================================
-
-// api ---------------------------------------------------------------------
-// get all todos
-app.get('/api/todos', function(req, res) {
-
-    // use mongoose to get all todos in the database
-    Todo.find(function(err, todos) {
-
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err)
-            res.send(err)
-
-        res.json(todos); // return all todos in JSON format
-    });
-});
-
-// create todo and send back all todos after creation
-app.post('/api/todos', function(req, res) {
-
-    // create a todo, information comes from AJAX request from Angular
-    Todo.create({
-        text : req.body.text,
-        done : false
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err)
-            res.json(todos);
-        });
-    });
-
-});
-
-// delete a todo
-app.delete('/api/todos/:todo_id', function(req, res) {
-    Todo.remove({
-        _id : req.params.todo_id
-    }, function(err, todo) {
-        if (err)
-            res.send(err);
-
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos) {
-            if (err)
-                res.send(err)
-            res.json(todos);
-        });
-    });
-});
 
 // application -------------------------------------------------------------
 app.get('*', function(req, res) {
